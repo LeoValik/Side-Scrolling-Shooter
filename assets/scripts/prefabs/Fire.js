1d0d9bbf947d870e9b8732f4009f245520c35bbc
Valentyn Troian <troval12989@gmail.com>
2022-05-28 10:16:55 +0300

    Refactoring of code

--- assets/scripts/prefabs/Fire.js
+++ assets/scripts/prefabs/Fire.js
@@ -1,8 +1,4 @@
-class Fire extends Phaser.GameObjects.Sprite {
-    constructor(data) {
-        super(data.scene, data.x, data.y, data.texture);
-        this.init(data);
-    }
+class Fire extends MovableObject {
     static generate(scene, source) {
         const data = {
             scene,
@@ -13,30 +9,7 @@ class Fire extends Phaser.GameObjects.Sprite {
         }
         return new Fire(data);
     }
-    reset(source) {
-        this.x = source.x + source.width / 2;
-        this.y = source.y;
-        this.setAlive(true);
-    }
-    init(data) {
-        this.scene.add.existing(this);
-        this.velocity = data.velocity;
-        this.scene.events.on('update', this.update, this);
-    }
-    update() {
-        if(this.active && (this.x < -this.width || this.x > config.width + this.width)) {
-            this.setAlive(false);
-        }   
-    }
-    setAlive(status) {
-        // activate OR deactivate physical body
-        this.body.enable = status;
-        // hide OR show texture
-        this.setVisible(status);
-        // deactivate OR activate object
-        this.setActive(status);
-    }
-    move() {
-        this.body.setVelocityX(this.velocity);
+    isDead() {
+        return this.x < -this.width || this.x > config.width + this.width;
     }
 }
