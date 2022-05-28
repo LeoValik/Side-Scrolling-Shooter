777ea8210456012226084a2595bc740166499383
Valentyn Troian <troval12989@gmail.com>
2022-05-28 09:46:47 +0300

    Recreate objects fire in group

--- assets/scripts/prefabs/Fire.js
+++ assets/scripts/prefabs/Fire.js
@@ -13,12 +13,28 @@ class Fire extends Phaser.GameObjects.Sprite {
         }
         return new Fire(data);
     }
-    reset() {
-        
+    reset(source) {
+        this.x = source.x + source.width / 2;
+        this.y = source.y;
+        this.setAlive(true);
     }
     init(data) {
         this.scene.add.existing(this);
         this.velocity = data.velocity;
+        this.scene.events.on('update', this.update, this);
+    }
+    update() {
+        if(this.active && (this.x < -this.width || this.x > config.width + this.width)) {
+            this.setAlive(false);
+        }   
+    }
+    setAlive(status) {
+        // activate OR deactivate physical body
+        this.body.enable = status;
+        // hide OR show texture
+        this.setVisible(status);
+        // deactivate OR activate object
+        this.setActive(status);
     }
     move() {
         this.body.setVelocityX(this.velocity);
